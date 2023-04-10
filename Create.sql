1f839db3a4aa739882d27b4bb1f59f1c43cc44b1
townplural <113643153+townplural@users.noreply.github.com>
2023-04-10 16:48:30 +0300

    Update Create.sql

--- Create.sql
+++ Create.sql
@@ -5,7 +5,6 @@ create table if not exists List_of_genres (
 
 create table if not exists List_of_artists (
 	artist_id serial primary key,
-	album_id INTEGER,
 	name text
 );
 
@@ -48,8 +47,3 @@ create table if not exists TrackCompilation (
 	primary key (track_id, compilation_id)
 );
 
-create table if not exists AlbumTrack (
-	track_id INTEGER references List_of_tracks (track_id),
-	album_id integer references List_of_albums (album_id),
-	primary key (track_id, album_id)
-);
